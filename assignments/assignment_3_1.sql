dd30507c58d409bbfcdab42e40274b713cd6a78e
Arie Twigt <ariekanarie@twigtd4.nl>
2023-07-24 15:05:41 +0200

    corrected 3_1

--- assignments/assignment_3_1.sql
+++ assignments/assignment_3_1.sql
@@ -1,5 +1,5 @@
 -- Create a selection in which you can find the car and model with the highest tax
-SELECT cars.brand, cars.model, cars.price
+SELECT cars.brand, cars.model, cars.tax
 FROM registered_cars AS cars
-ORDER BY cars.price DESC
+ORDER BY cars.tax DESC
 LIMIT 5
