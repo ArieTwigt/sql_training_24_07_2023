d2992ddae88c2389ab106c0d8c8ebef1422045d4
Arie Twigt <ariekanarie@twigtd4.nl>
2023-07-27 15:34:18 +0200

    added assignment 5_1

--- assignments/assignment_5_1.sql
+++ assignments/assignment_5_1.sql
@@ -1,18 +1,18 @@
 -- Create a query in which you provide an overview of the most popular colours for each country (of origin)
+
 WITH cars_color AS (
-    SELECT cars.color,
-       brands.country,
-       COUNT(*) as total
+    SELECT brands.country,
+        cars.color,
+        COUNT(*) AS total
     FROM registered_cars AS cars
     JOIN car_brands AS brands
     ON cars.brand = brands.brand
     GROUP BY 1,2
-    ORDER BY 3 DESC
 )
 
-SELECT cars_color.country,
+SELECT cars_color.country, 
        cars_color.color,
-       MAX(cars_color.total) AS total
+       MAX(cars_color.total) AS max_total
 FROM cars_color
 GROUP BY 1
 ORDER BY 3 DESC
